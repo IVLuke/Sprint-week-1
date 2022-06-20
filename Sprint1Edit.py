fa08fc01d2728a4bc2a103e863f942e6c8611337
IVLuke <107568934+IVLuke@users.noreply.github.com>
2022-06-20 19:05:01 -0230

    Revert "Update Sprint1Edit.py"

--- Sprint1Edit.py
+++ Sprint1Edit.py
@@ -1,13 +1,3 @@
-# Employee work trip claims processing for NL Chocolate Company
-# Authors: Nathan Greene & Luke Jones
-# Date Submitted:
-
-import math
-import matplotlib.pyplot as plt
-import datetime
-
-# Functions
-
 def EmpTravelClaim():
     return
 def FunIntQuestion():
@@ -19,24 +9,10 @@ def FunIntQuestion():
         elif List % 8 == 0:
             List = "Buzz"
         print(List)
-        
 def StringAndDate():
     return
 def GraphingMonthClaim():
-    x_axis = XList
-    y_axis = YList
-
-    plt.title("Total Sales for Each Month")
-    plt.scatter(x_axis, y_axis, color='darkred', marker='x',)
-
-    plt.plot(x_axis, y_axis)
-
-    plt.xlabel("Time (Months)")
-    plt.ylabel("Sale Totals (Dollars)")
-
-    plt.grid(True)
-    plt.show()
-    
+    return
 def QuitProgram():
     print("You have quit the program!")
 
@@ -78,23 +54,6 @@ while True:
         AnyKey = input("Press any key to continue")
         print()
     elif Choice == 4:
-        print()
-        NumInputs = 1
-        XList = []
-        YList = []
-        while NumInputs <= 12:
-            Month = str(NumInputs)
-            Month = datetime.datetime.strptime(Month, "%m")
-            FullMonth = Month.strftime("%B")
-            MedMonth = Month.strftime("%b")
-            try:
-                MonthlyTotal = float(input(f"Enter the total monthly sales for {FullMonth}: "))
-            except:
-                print("Error - Must enter a number value")
-                continue
-            NumInputs += 1
-            XList.append(MedMonth)
-            YList.append(MonthlyTotal)
         GraphingMonthClaim()
         print()
         AnyKey = input("Press any key to continue")
@@ -102,3 +61,4 @@ while True:
     elif Choice == 5:
         QuitProgram()
         break
+
